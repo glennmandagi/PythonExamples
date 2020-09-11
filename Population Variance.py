c40bbc8ed8d7b5316c6239f8bf9e6e6b82eca5fb
Glenn Mandagi <s21710491@student.unklab.ac.id>
2020-09-11 18:02:37 +0800

    Create Population Variance.py

---
+++ Population Variance.py
@@ -0,0 +1,12 @@
+# call numpy package
+import numpy as np
+
+# input data
+x = [77, 50, 99, 60, 90, 82, 93, 75, 86, 99, 55, 79]
+
+# numpy.var method
+get_population_var = np.var(x)
+
+# output
+print("Input x : ", x)
+print("Population Variance of x : ", get_population_var)
